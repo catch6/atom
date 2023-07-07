f51ebaf629ee6b54173cd8a52910b7aec967ade3
Catch <catchlife6@163.com>
2023-07-07 13:38:08 +0800

    新增: 常用请求响应体,如:IdName,IdRequest,OptionResponse

---
+++ atom-spring-boot-starter-core/src/main/java/net/wenzuo/atom/core/param/IdRequest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022-2023 Catch
+ * [Atom] is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+package net.wenzuo.atom.core.param;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Catch
+ * @since 2023-07-07
+ */
+@Data
+public class IdRequest {
+
+	@Schema(description = "ID")
+	@NotNull(message = "ID不能为空")
+	private Long id;
+
+}
---
+++ atom-spring-boot-starter-core/src/main/java/net/wenzuo/atom/core/param/OptionResponse.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022-2023 Catch
+ * [Atom] is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+package net.wenzuo.atom.core.param;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author Catch
+ * @since 2023-07-07
+ */
+@Data
+public class OptionResponse {
+
+	@Schema(description = "选项列表")
+	private List<IdName> items;
+
+}

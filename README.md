e2eaae200711695aa112c3210bdb5326abe3d393
Catch <catchlife6@163.com>
2023-07-21 13:59:23 +0800

    新版本: 2.0.12

--- README.md
+++ README.md
@@ -44,7 +44,7 @@ Atom 是一个基于 SpringBoot 和 SpringCloud 的企业级常用组件封装�
 <parent>
 	<groupId>net.wenzuo</groupId>
 	<artifactId>atom</artifactId>
-	<version>2.0.11</version>
+	<version>2.0.12</version>
 	<relativePath/>
 </parent>
 ```
--- atom.sh
+++ atom.sh
@@ -21,110 +21,118 @@ ACTION=$1
 VERSION=$2
 
 function info() {
-  echo -e "\033[32m$1\033[0m"
+    echo -e "\033[32m$1\033[0m"
 }
 
 function error() {
-  echo -e "\033[31m$1\033[0m"
+    echo -e "\033[31m$1\033[0m"
+}
+
+function setCurrentVersion() {
+    VERSION=$(awk -F'[><]' '/<revision>/{print $3}' pom.xml)
 }
 
 if [ "${ACTION}" == "version" ]; then
-  info "参数校验..."
-  if [ -z "${VERSION}" ] ;then
-      error "请输入版本号! 示例: 1.0.0"
-      exit
-  fi
-  info "参数校验...OK!\n"
+    info "参数校验..."
+    if [ -z "${VERSION}" ]; then
+        error "请输入版本号! 示例: 1.0.0"
+        exit
+    fi
+    info "参数校验...OK!\n"
 
-  info "修改 README.md 版本号..."
-  sed -i '' "s/<version>.*<\/version>/<version>${VERSION}<\/version>/g" README.md
-  info "修改 README.md 版本号...OK!\n"
+    info "修改 README.md 版本号..."
+    sed -i '' "s/<version>.*<\/version>/<version>${VERSION}<\/version>/g" README.md
+    info "修改 README.md 版本号...OK!\n"
 
-  info "更新 pom.xml 中 revision 版本号..."
-  sed -i '' "s/<revision>.*<\/revision>/<revision>${VERSION}<\/revision>/g" pom.xml
-  info "更新 pom.xml 中 revision 版本号...OK!\n"
+    info "更新 pom.xml 中 revision 版本号..."
+    sed -i '' "s/<revision>.*<\/revision>/<revision>${VERSION}<\/revision>/g" pom.xml
+    info "更新 pom.xml 中 revision 版本号...OK!\n"
 
-  info "版本${VERSION}设置完成!"
+    info "版本${VERSION}设置完成!"
 elif [ "${ACTION}" == "deploy" ]; then
-  info "参数校验..."
-  if [ -z "${VERSION}" ] ;then
-      error "请输入版本号! 示例: 1.0.0"
-      exit
-  fi
-  info "参数校验...OK!\n"
+    if [ -z "${VERSION}" ]; then
+        setCurrentVersion
+    fi
+    info "发布版本 ${VERSION} ..."
 
-  info "检查本地是否有代码未提交..."
-  if [ -n "$(git status -s)" ] ;then
-      error "本地有未提交的代码!"
-      exit
-  fi
-  info "检查本地是否有代码未提交...OK!\n"
+    info "检查本地是否有代码未提交..."
+    if [ -n "$(git status -s)" ]; then
+        error "本地有未提交的代码!"
+        exit
+    fi
+    info "检查本地是否有代码未提交...OK!\n"
 
-  info "切换到 main 分支..."
-  git checkout main
-  info "切换到 main 分支...OK!\n"
+    info "切换到 main 分支..."
+    git checkout main
+    info "切换到 main 分支...OK!\n"
 
-  info "拉取 main 分支..."
-  git pull origin main
-  info "拉取 main 分支...OK!\n"
+    info "拉取 main 分支..."
+    git pull origin main
+    info "拉取 main 分支...OK!\n"
 
-  info "合并 2.x 分支到 main 分支..."
-  git merge 2.x
-  info "合并 2.x 分支到 main 分支...OK!\n"
+    info "合并 2.x 分支到 main 分支..."
+    git merge 2.x
+    info "合并 2.x 分支到 main 分支...OK!\n"
 
-  info "修改 README.md 版本号..."
-  sed -i '' "s/<version>.*<\/version>/<version>${VERSION}<\/version>/g" README.md
-  info "修改 README.md 版本号...OK!\n"
+    info "发布到 maven..."
+    mvn -Possrh -Prelease clean deploy -U -DskipTests
+    info "发布到 maven...OK!\n"
 
-  info "更新 pom.xml 中 revision 版本号..."
-  sed -i '' "s/<revision>.*<\/revision>/<revision>${VERSION}<\/revision>/g" pom.xml
-  info "更新 pom.xml 中 revision 版本号...OK!\n"
+    info "提交代码并推送..."
+    git add .
+    git commit -m "发布 ${VERSION}"
+    git push origin main
+    info "提交代码并推送...OK!\n"
 
-  info "发布到 maven..."
-  mvn -Possrh -Prelease clean deploy -U -DskipTests
-  info "发布到 maven...OK!\n"
+    info "检查tag是否存在..."
+    if git rev-parse "${VERSION}" >/dev/null 2>&1; then
+        info "tag存在!"
+        info "删除本地旧的 tag..."
+        git tag -d "${VERSION}"
+        info "删除本地旧的 tag...OK!\n"
 
-  info "提交代码并推送..."
-  git add .
-  git commit -m "发布 ${VERSION}" ||
-  info "提交代码并推送...OK!\n"
+        info "删除远程旧的 tag..."
+        git push --delete origin "${VERSION}"
+        info "删除远程旧的 tag...OK!\n"
+    fi
 
-  info "推送分支..."
-  git push origin main
-  info "推送分支...OK!\n"
+    info "创建 tag..."
+    git tag -a "${VERSION}" -m "${VERSION}"
+    info "创建 tag...OK!\n"
 
-  info "检查tag是否存在..."
-  if git rev-parse "${VERSION}" >/dev/null 2>&1; then
-    info "tag存在!"
-    info "删除本地旧的 tag..."
-    git tag -d "${VERSION}"
-    info "删除本地旧的 tag...OK!\n"
+    info "推送 tag..."
+    git push origin "${VERSION}"
+    info "推送 tag...OK!\n"
 
-    info "删除远程旧的 tag..."
-    git push --delete origin "${VERSION}"
-    info "删除远程旧的 tag...OK!\n"
-  fi
+    info "切换到 2.x 分支..."
+    git checkout 2.x
+    info "切换到 2.x 分支...OK!\n"
 
-  info "创建 tag..."
-  git tag -a "${VERSION}" -m "${VERSION}"
-  info "创建 tag...OK!\n"
+    info "将 main 分支合并到 2.x 分支..."
+    git merge main
+    info "将 main 分支合并到 2.x 分支...OK!\n"
 
-  info "推送 tag..."
-  git push origin "${VERSION}"
-  info "推送 tag...OK!\n"
+    info "新版本..."
+    MAJOR=$(cut -d. -f1 <<< "${VERSION}")
+    MINOR=$(cut -d. -f2 <<< "${VERSION}")
+    PATCH=$(cut -d. -f3 <<< "${VERSION}")
+    ((PATCH++))
+    NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}"
+    info "新版本 ${NEW_VERSION} ...OK!\n"
 
-  info "切换到 2.x 分支..."
-  git checkout 2.x
-  info "切换到 2.x 分支...OK!\n"
+    info "修改 README.md 版本号..."
+    sed -i '' "s/<version>.*<\/version>/<version>${NEW_VERSION}<\/version>/g" README.md
+    info "修改 README.md 版本号...OK!\n"
 
-  info "将 main 分支合并到 2.x 分支..."
-  git merge main
-  info "将 main 分支合并到 2.x 分支...OK!\n"
+    info "更新 pom.xml 中 revision 版本号..."
+    sed -i '' "s/<revision>.*<\/revision>/<revision>${NEW_VERSION}<\/revision>/g" pom.xml
+    info "更新 pom.xml 中 revision 版本号...OK!\n"
 
-  info "推送 2.x 分支..."
-  git push origin 2.x
-  info "推送 2.x 分支...OK!\n"
+    info "提交代码并推送..."
+    git add .
+    git commit -m "新版本: ${NEW_VERSION}"
+    git push origin 2.x
+    info "提交代码并推送...OK!\n"
 
-  echo "进行下一版本的开发,请设置新版本version..."
+    echo "进行 ${NEW_VERSION} 版本的开发..."
 fi
-

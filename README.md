9a702a105a4ae43710ba57d8907334a0ba2664cf
Catch <catchlife6@163.com>
2023-08-13 22:26:51 +0800

    新增: Kafka 模块

--- README.md
+++ README.md
@@ -34,6 +34,7 @@ Atom 是一个基于 SpringBoot 和 SpringCloud 的企业级常用组件封装�
 - [atom-spring-boot-starter-web](atom-spring-boot-starter-web) Web 模块
 - [atom-spring-cloud-starter-consul](atom-spring-cloud-starter-consul) Consul 模块
 - [atom-spring-cloud-starter-feign](atom-spring-cloud-starter-feign) Feign 模块
+- [atom-spring-cloud-starter-kafka](atom-spring-cloud-starter-kafka) Kafka 模块
 - [atom-spring-cloud-starter-nacos-config](atom-spring-cloud-starter-nacos-config) Nacos Config 模块
 - [atom-spring-cloud-starter-nacos-discovery](atom-spring-cloud-starter-nacos-discovery) Nacos Discovery 模块
 
---
+++ atom-spring-cloud-starter-kafka/src/main/java/net/wenzuo/atom/kafka/service/impl/KafkaServiceImpl.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2022-2023 Catch(catchlife6@163.com).
+ * Atom is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+package net.wenzuo.atom.kafka.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.wenzuo.atom.core.util.JsonUtils;
+import net.wenzuo.atom.kafka.service.KafkaService;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author Catch
+ * @since 2023-08-13
+ */
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class KafkaServiceImpl implements KafkaService {
+
+	private final KafkaTemplate<String, String> kafkaTemplate;
+
+	@Override
+	public void send(String topic, Object message) {
+		send(topic, null, message);
+	}
+
+	@Override
+	public void send(String topic, String key, Object message) {
+		// 分区编号为 null，交给 Kafka 自己去分配
+		ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, null, System.currentTimeMillis(), key, JsonUtils.toJson(message));
+		CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(producerRecord);
+		future.whenComplete((result, ex) -> {
+			if (ex == null) {
+				log.info("Kafka 消息发送成功, topic:{}, partition:{}", result.getRecordMetadata().topic(), result.getRecordMetadata().partition());
+			} else {
+				log.error("Kafka 消息发送失败: {}", ex.getMessage());
+			}
+		});
+	}
+
+}

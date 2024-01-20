cc5c181329f48a76b74f730b8c10554566e68481
zhanghao <zh@cgreat.net>
2024-01-21 01:43:09 +0800

    修改: web 对 OPTIONS 预请求不再打印日志

--- atom-spring-boot-starter-web/src/main/java/net/wenzuo/atom/web/config/LoggingFilter.java
+++ atom-spring-boot-starter-web/src/main/java/net/wenzuo/atom/web/config/LoggingFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2023 Catch(catchlife6@163.com).
+ * Copyright (c) 2022-2024 Catch(catchlife6@163.com).
  * Atom is licensed under Mulan PSL v2.
  * You can use this software according to the terms and conditions of the Mulan PSL v2.
  * You may obtain a copy of Mulan PSL v2 at:
@@ -54,10 +54,9 @@ import java.util.Map;
 public class LoggingFilter extends OncePerRequestFilter {
 
 	private static final ThreadLocal<Long> TIMER = new ThreadLocal<>();
-
-	private static final String REQ_ID = "Req-Id";
-
 	private static final PathMatcher PATH_MATCHER = new AntPathMatcher();
+	private static final String REQ_ID = "Req-Id";
+	private static final String OPTIONS = "OPTIONS";
 
 	@Resource
 	private LoggingProperties loggingProperties;
@@ -73,7 +72,12 @@ public class LoggingFilter extends OncePerRequestFilter {
 		if (!log.isInfoEnabled()) {
 			return true;
 		}
-		
+
+		String method = request.getMethod();
+		if (OPTIONS.equals(method)) {
+			return true;
+		}
+
 		// 排除 Server-Sent Events (SSE) 的请求
 		String accept = request.getHeader("Accept");
 		if (accept != null && accept.contains(MediaType.TEXT_EVENT_STREAM_VALUE)) {

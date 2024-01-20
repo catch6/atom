594153cdf41dbfec56ee3aebf072ea4199a4a739
zhanghao <zh@cgreat.net>
2024-01-21 01:42:30 +0800

    移除: core 移除 IdNamesResponse 可用 ItemsResponse 代替 新增: core 增加 ItemsResponse 用于通用列表响应

--- atom-spring-boot-starter-core/src/main/java/net/wenzuo/atom/core/param/ItemsResponse.java
+++ atom-spring-boot-starter-core/src/main/java/net/wenzuo/atom/core/param/ItemsResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2023 Catch(catchlife6@163.com).
+ * Copyright (c) 2022-2024 Catch(catchlife6@163.com).
  * Atom is licensed under Mulan PSL v2.
  * You can use this software according to the terms and conditions of the Mulan PSL v2.
  * You may obtain a copy of Mulan PSL v2 at:
@@ -19,12 +19,12 @@ import java.util.List;
 
 /**
  * @author Catch
- * @since 2023-07-07
+ * @since 2024-01-18
  */
 @Data
-public class IdNamesResponse {
+public class ItemsResponse<T> {
 
-	@Schema(description = "选项列表")
-	private List<IdName> items;
+	@Schema(description = "列表")
+	private List<T> items;
 
 }

99378db8ce32a92f184cc87aec52807bb22ddd05
zhanghao <catchlife6@163.com>
2025-08-28 01:10:38 +0800

    feat(web): 在响应头中添加 traceId
    - 在 TraceIdInterceptor 类中，于 preHandle 方法中添加了设置响应头的操作
    - 将 MDC 中的 traceId 添加到 HttpServletResponse 的头信息中- 优化了代码结构，调整了导入语句的顺序

--- atom-spring-boot-starter-web/src/main/java/cn/mindit/atom/web/core/TraceIdInterceptor.java
+++ atom-spring-boot-starter-web/src/main/java/cn/mindit/atom/web/core/TraceIdInterceptor.java
@@ -1,9 +1,9 @@
 package cn.mindit.atom.web.core;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import cn.mindit.atom.core.core.CoreConstants;
 import cn.mindit.atom.core.util.NanoIdUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.MDC;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
@@ -19,6 +19,7 @@ public class TraceIdInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) {
         MDC.put(CoreConstants.TRACE_ID, NanoIdUtils.nanoId());
+        response.setHeader(CoreConstants.TRACE_ID, MDC.get(CoreConstants.TRACE_ID));
         return true;
     }
 

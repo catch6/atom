cbe2f8877efb338649c88e34f653f0f11f279120
zhanghao <catchlife6@163.com>
2025-10-10 17:27:28 +0800

    feat(scheduling): 引入基于Redis的Shedlock分布式调度锁
    - 添加 atom-spring-boot-starter-scheduling-shedlock 依赖
    - 配置 Shedlock 使用 Redis作为锁存储
    - 设置默认锁超时时间为10分钟，最小持有时间为1秒
    - 启用调度锁功能并注册 LockProvider Bean
    - 更新自动配置类路径以启用新的调度锁配置

--- atom-spring-boot-starter-scheduling-shedlock/src/main/java/cn/mindit/atom/scheduling/shedlock/config/ShedAutoConfiguration.java
+++ atom-spring-boot-starter-scheduling-shedlock/src/main/java/cn/mindit/atom/scheduling/shedlock/config/ShedAutoConfiguration.java
@@ -1,9 +1,13 @@
 package cn.mindit.atom.scheduling.shedlock.config;
 
 import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -11,10 +15,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @since 2025-09-18
  */
 @EnableScheduling
-@EnableSchedulerLock(defaultLockAtMostFor = "50s")
+@EnableSchedulerLock(defaultLockAtLeastFor = "1s", defaultLockAtMostFor = "10m")
 @RequiredArgsConstructor
 @EnableConfigurationProperties(SchedulingShedlockProperties.class)
 @ConditionalOnProperty(value = "atom.scheduling.shedlock.enabled", matchIfMissing = true)
 public class ShedAutoConfiguration {
+
+    @Bean
+    public LockProvider lockProvider(RedisConnectionFactory connectionFactory) {
+        return new RedisLockProvider(connectionFactory);
+    }
 
 }
